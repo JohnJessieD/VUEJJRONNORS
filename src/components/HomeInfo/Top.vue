e693fd195b9202d9f9b39907a29ec203c1a6b98c
jj <jjdeniega377@gmail.com>
2023-10-13 21:22:07 +0800

    111

---
+++ src/components/HomeInfo/Top.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+
+        <nav>
+      <div class="navbar">
+        <div class="container nav-container">
+            <input class="checkbox" type="checkbox" name="" id="" />
+            <div class="hamburger-lines">
+              <span class="line line1"></span>
+              <span class="line line2"></span>
+              <span class="line line3"></span>
+            </div>
+
+          <div class="menu-items">
+            <ul id="links">
+                    <router-link to="/" tag="li" exact>Home</router-link><hr>
+                    <router-link to="/manage" tag="li"  exact>Manage</router-link><hr>
+                    <router-link to="/shop_info" tag="li" exact>Shop Info</router-link><hr>
+                <router-link to="/about" tag="li" exact>About</router-link>
+                </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
+          <div>
+            <div class="hero">
+                <div class="container">
+                    <div class="row justify-content-between">
+                        <div class="col-lg-5">
+                            <div class="intro-excerpt">
+
+                                <p class="mb-4">Badminton is a racquet sport played using racquets to hit a shuttlecock across a net. Although it may be played with larger teams, the most common forms of the game are "singles" and "doubles </p>
+                                <p><h5 href="/shop" class="btn btn-secondary me-2">Shop Now</h5></p>
+                            </div>
+                        </div>
+                            <div class="col-lg-7">
+                                <div class="hero-img-wrap">
+
+                                      </div>
+                                        <div class="hero-img-wrap">
+                                    <img :src="require('@/assets/assets/images/shoes1.png')" class="img-fluid">
+                                        </div>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    </div>
+    </template>
+
+    <script>
+      export default {
+        name: "Top",
+      data() {
+          return{
+          open: false,
+          }
+        },
+      methods: {
+
+          menubar: function() {
+            if(this.open){
+              this.open = false;
+              return this.open;
+            }else{
+              this.open = true;
+              return this.open;
+            }
+          }
+        },
+      };
+      //Ends
+    </script>
