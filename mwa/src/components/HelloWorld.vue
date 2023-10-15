64086270b0dc032beb8caebd657cedfecb996251
cjddz <130657061+Cjdudz@users.noreply.github.com>
2023-10-15 21:07:57 +0800

    1231

---
+++ mwa/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+ <div id="menubar">
+        <ul id="menu">
+          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+          <li class="selected"><a href="index.html">Home</a></li>
+          <li><a href="examples.html">Examples</a></li>
+          <li><a href="page.html">A Page</a></li>
+          <li><a href="another_page.html">Another Page</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a class="nav-link" href="/manage">Manage</a></li> 
+        </ul>
+      
+    </div>
+</template>
+
+<style>
+
+
+@import '@/assets/style/style.css';
+
+</style>
+
+<script>
+
+</script>
